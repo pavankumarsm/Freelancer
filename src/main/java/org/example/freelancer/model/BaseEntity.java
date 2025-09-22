d9071b541a91f87eb9550c87a9dda6092e2f1d5b
akashkumar1712 <shrirambhaktakash@gmail.com>
2025-09-22 23:57:19 +0530

    edit application property and add validation dependency

--- src/main/java/org/example/freelancer/model/BaseEntity.java
+++ src/main/java/org/example/freelancer/model/BaseEntity.java
@@ -1,7 +1,7 @@
 package org.example.freelancer.model;
 
 import jakarta.persistence.*;
-import org.antlr.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
