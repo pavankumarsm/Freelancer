3986cd9c987f63842be795c70a7dcb7fd10c63b6
Akash Kumar Singh <akash.singh.34.2@mountblue.tech>
2025-09-22 23:17:19 +0530

    main: added logs in main method to check write access in main branch

--- src/main/java/org/example/freelancer/FreelancerApplication.java
+++ src/main/java/org/example/freelancer/FreelancerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FreelancerApplication {
 
     public static void main(String[] args) {
+        System.out.println("Test code to check write access in repo");
         SpringApplication.run(FreelancerApplication.class, args);
     }
 
